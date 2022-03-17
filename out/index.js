7e14eb0584f83c4a3e43d4381970d89ee411e915
shaguly <dasoguzwelayat95@gmail.com>
2022-03-17 14:53:29 +0500

    toza qoshildi

---
+++ out/index.js
@@ -0,0 +1,24 @@
+"use strict";
+let kunUz = {
+    name: 'kun.uz',
+    date: '15.03.2022',
+    img: 'kun.uz img',
+    https: 'https://kun.uz/',
+    text: 'Маст ҳолда рулга ўтирганлар 5 йилга ҳайдовчилик ҳуқуқидан маҳрум қилиниши мумкин',
+    textAvtor: 'Boltaboy Muratov',
+    MaqolaAvtori: {
+        name: 'Boltaboy',
+        userName: 'Muratov',
+        profession: 'jurnalist',
+        img: 'Boltaboy.img',
+        rating: '1-o`rinda'
+    }
+};
+console.log(kunUz);
+let sinfMaktab = {
+    direction: 'Biologiya',
+    class: '9b sinf',
+    studentsName: ['Boltaboy', 'Azizbek', 'Yarash'],
+    teacherClass: 'Ozoda Sobirova'
+};
+console.log(sinfMaktab);
